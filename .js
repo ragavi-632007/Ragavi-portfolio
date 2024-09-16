b59779d930d9c015dfb7a5b9fa42970f0473d32a
ragavi-632007 <ragavi06032007@gmail.com>
2024-09-16 17:20:38 +0530

    first commit

---
+++ .js
@@ -0,0 +1,10 @@
+document.addEventListener(
+    'DOMContentLoaded',()=>
+    {
+        const emailElement=document.getElement=document.getElementById('email');
+        emailElement.addEventListener('click',()=>
+            {
+            alter('Contact me at:ragavi06032007@gmail.com');
+        });
+    }
+);
